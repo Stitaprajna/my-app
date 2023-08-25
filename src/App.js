b107cc91b7d1a1807d6edd77a7eae7792831c2bc
Stitaprajna <stitaprajna.panda@gmail.com>
2023-08-25 19:32:33 +0530

    Created AI-Chatbot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChatbotApp } from "./pages/chatbot.js"
+import { Login } from "./pages/login.js"
+import { BrowserRouter as Router, Route,Link,Routes,Switch } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div className="App">
+  <Router>
+    <Routes>
+      <Route exact path="/Login" Component={Login}/>
+      <Route exact path="/Chatbot" element={<ChatbotApp/>}/>
+    </Routes>
+  </Router>
+  </div> 
+
+)}
 
 export default App;
